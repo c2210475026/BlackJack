64a99ff62917050d8f0e7f4db322836e410f2ea3
alexandermessner <alexander.messner1@stud.fh-campuswien.ac.at>
2022-12-13 14:53:24 +0100

    TESTING programming branch

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -3,5 +3,6 @@ package at.ac.fhcampuswien;
 public class App {
     public static void main(String[] args) {
         System.out.println("TEST");
+        System.out.println("PROGRAMMING");
     }
 }
