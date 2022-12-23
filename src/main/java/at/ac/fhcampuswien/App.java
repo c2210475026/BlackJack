79a40011c4c09e8277e32409a6ac4002c71978e5
James Ponce <jamesponce10@outlook.com>
2022-12-23 19:37:56 +0100

    programmierte Funktionen funktionieren Bei "Testing" eigene Testingklasse hinzugefügt

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -2,8 +2,6 @@ package at.ac.fhcampuswien;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("TEST");
-        System.out.println("PROGRAMMING");
-        System.out.println("TSET");
+
     }
 }
