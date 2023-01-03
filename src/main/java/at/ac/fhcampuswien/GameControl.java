60ee18d0e85dc1dc60a0b808311fa998e9ad703a
Lindrit14 <Lindrit_lindi@hotmail.com>
2023-01-03 19:49:59 +0100

    set up GameControl.java and add a resolveWinner() method

--- src/main/java/at/ac/fhcampuswien/GameControl.java
+++ src/main/java/at/ac/fhcampuswien/GameControl.java
@@ -1,4 +1,72 @@
 package at.ac.fhcampuswien;
 
+import java.util.Scanner;
+
 public class GameControl {
-}
+
+    Scanner sc = new Scanner(System.in);
+
+
+    //Variables
+    private Player player;
+
+    private Dealer dealer;
+
+
+    //Constructor
+    public GameControl(Player player, Dealer dealer) {
+        this.player = player;
+        this.dealer = dealer;
+    }
+
+    //Getter and Setter
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Dealer getDealer() {
+        return dealer;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public void setDealer(Dealer dealer) {
+        this.dealer = dealer;
+    }
+
+    //Methods
+
+
+    public Person resolveWinner(Player player, Dealer dealer){
+
+        int playersHandSum = player.getCurrentHand().sumOfCards();
+
+        int dealersHandSum = dealer.getCurrentHand().sumOfCards();
+
+        if (playersHandSum > dealersHandSum){
+            return player;
+        }else if (playersHandSum < dealersHandSum){
+            return dealer;
+        }else {
+            throw new RuntimeException("It's a Tie");
+        }
+    }
+
+
+    public void playRound(){
+        //Persons draw their Hand
+
+
+        //Output the Cards of the Persons
+
+
+
+
+    }
+
+
+
+}//end of class
+
--- src/main/java/at/ac/fhcampuswien/Player.java
+++ src/main/java/at/ac/fhcampuswien/Player.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Player extends Person {
 
+    private int coins;
+
     Scanner sc = new Scanner(System.in);
 
 
